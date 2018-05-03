9cd28883df919b6881d96ca54307fbd63eca53c1
Alexus McGriff <mcgriffale@gmail.com>
2018-05-02 20:32:37 -0400

    Created and added onCreate in CrimeFragment class.

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -16,31 +16,31 @@ public class Crime {
         mDate = new Date();
     }
 
-    public UUID getmId() {
+    public UUID getId() {
         return mId;
     }
 
-    public String getmTitle() {
+    public String getTitle() {
         return mTitle;
     }
 
-    public Date getmDate() {
+    public Date getDate() {
         return mDate;
     }
 
-    public boolean ismSolved() {
+    public boolean isSolved() {
         return mSolved;
     }
 
-    public void setmTitle(String mTitle) {
+    public void setTitle(String mTitle) {
         this.mTitle = mTitle;
     }
 
-    public void setmDate(Date mDate) {
+    public void setDate(Date mDate) {
         this.mDate = mDate;
     }
 
-    public void setmSolved(boolean mSolved) {
+    public void setSolved(boolean mSolved) {
         this.mSolved = mSolved;
     }
 }
