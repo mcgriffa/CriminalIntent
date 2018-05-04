d6a5918cf9c59c25e0b2d047f6b8378c30a0fe5b
Alexus McGriff <mcgriffale@gmail.com>
2018-05-04 10:23:28 -0400

    Made every other crime a crime that requires the police.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -25,6 +25,7 @@ public class CrimeLab {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0); // Every other one
+            crime.setRequiresPolice(i % 2 == 0); //Every other one
             mCrimes.add(crime);
         }
     }
